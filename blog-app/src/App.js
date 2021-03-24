ff453273887f7fd491bca1b3be1d8946bc91e707
Anel Gabriel Dominguez <anelgabriel002@gmail.com>
2021-03-24 12:47:16 -0400

    Add files via upload

---
+++ blog-app/src/App.js
@@ -0,0 +1,18 @@
+import { Route } from 'react-router-dom';
+import './assets/css/App.css';
+
+import Router from "./Router";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="contenedor">
+      
+        <Router></Router>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ blog-app/src/Global.js
@@ -0,0 +1,6 @@
+var Global = {
+    url: "https://frozen-retreat-03683.herokuapp.com/api/"
+}
+
+
+export default Global;
